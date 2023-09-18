4bde5605d9f7b7b6048dc316831d61d038738bdb
jiushi21 <zc931325671@gmail.com>
2023-09-18 14:51:22 +0800

    多种provider注入对象

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,43 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Inject } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
+  //inject的方式
+  @Inject('parent')
+  private readonly parent: {
+    name: string;
+    age: number;
+  };
+  @Inject('parent2')
+  private readonly parent2: {
+    name: string;
+    desc: string;
+  };
+  @Inject('parent3')
+  private readonly parent3: {
+    name: string;
+    appService: string;
+  };
+  @Inject('parent5')
+  private readonly parent5: {
+    name: string;
+    desc: string;
+  };
+  @Inject('parent4')
+  private readonly parent4: {
+    name: string;
+    desc: string;
+  };
   @Get()
   getHello(): string {
+    console.log(this.parent);
+    console.log(this.parent2);
+    console.log(this.parent3);
+    console.log(this.parent4);
+    console.log(this.parent5);
+    // debugger;
     return this.appService.getHello();
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,55 @@ import { AppService } from './app.service';
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  // providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: AppService,
+      useClass: AppService,
+    },
+    {
+      provide: 'parent',
+      useValue: {
+        name: 'parent',
+        age: 30,
+      },
+    },
+    {
+      provide: 'parent2',
+      useFactory() {
+        return {
+          name: 'parent2',
+          desc: 'zhaoyu',
+        };
+      },
+    },
+    {
+      provide: 'parent3',
+      useFactory(parent: { name: string }, appService: AppService) {
+        return {
+          name: parent.name,
+          appService: appService.getHello(),
+        };
+      },
+      inject: ['parent', AppService],
+    },
+    {
+      provide: 'parent4',
+      useExisting: 'parent2',
+    },
+    {
+      provide: 'parent5',
+      async useFactory() {
+        await new Promise((resolve) => {
+          setTimeout(resolve, 1000);
+        });
+        return {
+          name: 'aaa',
+          desc: 'ccc',
+        };
+      },
+    },
+  ],
 })
 export class AppModule {}
